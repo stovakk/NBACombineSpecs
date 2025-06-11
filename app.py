290826398e1677a09995773c40587b1a80dc0bf8
Nate S <natestovak@gmail.com>
2025-06-11 16:34:13 -0700

    redo app.py for render.com

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-import sys
-import traceback
-
-try:
-    from dashboard import app
-except Exception:
-    traceback.print_exc(file=sys.stderr)
-    sys.exit(1)
+import os
+from dashboard import app
 
 if __name__ == "__main__":
-    try:
-        app.run(host="0.0.0.0", port=8080)
-    except Exception:
-        traceback.print_exc(file=sys.stderr)
-        sys.exit(1)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
