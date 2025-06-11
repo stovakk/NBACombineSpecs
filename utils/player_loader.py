31e09f0bfbbae62aacd20b0d6cdf8d51afbd34c6
Nate S <natestovak@gmail.com>
2025-06-11 14:43:03 -0700

    change .csv so it will be compatible with aws

--- utils/player_loader.py
+++ utils/player_loader.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-def load_custom_players(path = "Utah Players Anthro & Combine(Sheet1).csv"):
+def load_custom_players(path = "utah_players.csv"):
     df = pd.read_csv(path)
     return df
