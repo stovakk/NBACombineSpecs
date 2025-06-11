8902360f62b8155ffd61b0b526b385a0658297fe
Nate S <natestovak@gmail.com>
2025-06-11 16:53:10 -0700

    redo app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from dashboard import app
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=False, host="0.0.0.0", port=port)
