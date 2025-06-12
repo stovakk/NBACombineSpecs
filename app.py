a460892409ba39c6b1d981f39876755d740837a3
Nate S <natestovak@gmail.com>
2025-06-11 17:12:26 -0700

    small changes

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from dashboard import app
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
